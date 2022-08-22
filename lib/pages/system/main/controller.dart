0df6360d80eccd981f23b5503b599403eba7c69d
WinUser01 <WinUser01@163.com>
2022-08-22 23:27:30 +0800

    flutter_woo_commerce_getx_learn-Commit009: 按照 01-4-models.mp4 进行下列操作
    （1）在VScode中，接着 Commit008 继续操作，在左侧项目文件列表中右击项目目录，选择 Getx: StatefulWidget + GetBuilder Page 创建好下列页面：
    一层	二层			说明
    system	main			主界面
    
    由于main主界面包含导航和Tab切换，所以需要以这种方式创建，其实就是 StatefulWidget 包裹了一个 GetBuilder
    
    （2）删除 Commit008 中 system 目录下创建错误的splash、welcome，重新选择 Getx: GetBuilderPage 创建好下列页面：
    一层	二层			说明
    system	splash			首屏
    		welcome			欢迎

---
+++ lib/pages/system/main/controller.dart
@@ -0,0 +1,27 @@
+import 'package:get/get.dart';
+
+class MainController extends GetxController {
+  MainController();
+
+  _initData() {
+    update(["main"]);
+  }
+
+  void onTap() {}
+
+  // @override
+  // void onInit() {
+  //   super.onInit();
+  // }
+
+  @override
+  void onReady() {
+    super.onReady();
+    _initData();
+  }
+
+  // @override
+  // void onClose() {
+  //   super.onClose();
+  // }
+}
---
+++ lib/pages/system/main/view.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'index.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage>
+    with AutomaticKeepAliveClientMixin {
+  @override
+  bool get wantKeepAlive => true;
+
+  @override
+  Widget build(BuildContext context) {
+    super.build(context);
+    return const _MainViewGetX();
+  }
+}
+
+class _MainViewGetX extends GetView<MainController> {
+  const _MainViewGetX({Key? key}) : super(key: key);
+
+  // 主视图
+  Widget _buildView() {
+    return const Center(
+      child: Text("MainPage"),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GetBuilder<MainController>(
+      init: MainController(),
+      id: "main",
+      builder: (_) {
+        return Scaffold(
+          appBar: AppBar(title: const Text("main")),
+          body: SafeArea(
+            child: _buildView(),
+          ),
+        );
+      },
+    );
+  }
+}
