36788c8cbef14fb73a20e10201a397952e27c865
WinUser01 <WinUser01@163.com>
2022-08-25 01:06:12 +0800

    Commit038: 按照 05-3-bar.mp4 进行操作，已经实现 Skip、Next、Start 按钮联动

--- lib/pages/system/welcome/controller.dart
+++ lib/pages/system/welcome/controller.dart
@@ -1,17 +1,30 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter_woo_commerce_getx_learn/common/index.dart';
 import 'package:get/get.dart';
 
 class WelcomeController extends GetxController {
   WelcomeController();
 
-  List<WelcomeModel>? items;
-  // 当前位置
-  int currentIndex = 0;
+  List<WelcomeModel>? items; // 滚动集合
+  int currentIndex = 0; // 当前项
+  bool isShowStart = false; // 是否显示 Start
+  CarouselController carouselController = CarouselController(); // slider 控制器
 
   // 当前位置发生改变
   void onPageChanged(int index) {
     currentIndex = index;
+    isShowStart = currentIndex == 2;
     update(['slider', 'bar']);
+  }
+
+  // 去首页
+  void onToMain() {
+    Get.offAllNamed(RouteNames.systemMain);
+  }
+
+  // 下一页
+  void onNext() {
+    carouselController.nextPage();
   }
 
   _initData() {
--- lib/pages/system/welcome/view.dart
+++ lib/pages/system/welcome/view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_woo_commerce_getx_learn/common/index.dart';
 import 'package:get/get.dart';
 
@@ -16,26 +17,47 @@ class WelcomePage extends GetView<WelcomeController> {
           ? const SizedBox()
           : WelcomeSliderWidget(
               controller.items!,
+              carouselController: controller.carouselController,
               onPageChanged: controller.onPageChanged,
             ),
     );
   }
 
-  // 控制栏
+  // 控制栏：skip + indicator + next
   Widget _buildBar() {
     return GetBuilder<WelcomeController>(
       id: "bar",
       init: controller,
       builder: (controller) {
-        return <Widget>[
-          // 指示标
-          SliderIndicatorWidget(
-            length: 3,
-            currentIndex: controller.currentIndex,
-          ),
-        ].toRow(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-        );
+        return controller.isShowStart
+            ?
+            // 开始
+            ButtonWidget.primary(
+                LocaleKeys.welcomeStart.tr,
+                onTap: controller.onToMain,
+              ).tight(
+                width: double.infinity,
+                height: 50.h,
+              )
+            : <Widget>[
+                // 跳过
+                ButtonWidget.text(
+                  LocaleKeys.welcomeSkip.tr,
+                  onTap: controller.onToMain,
+                ),
+                // 指示标
+                SliderIndicatorWidget(
+                  length: 3,
+                  currentIndex: controller.currentIndex,
+                ),
+                // 下一页
+                ButtonWidget.text(
+                  LocaleKeys.welcomeNext.tr,
+                  onTap: controller.onNext,
+                ),
+              ].toRow(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+              );
       },
     );
   }
